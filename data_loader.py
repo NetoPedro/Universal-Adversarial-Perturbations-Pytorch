0d16eb8384342609b898bd96acab42cbb186730a
NetoPedro <pedroneto_09@hotmail.com>
2019-06-06 18:47:48 +0100

    Moved Data_Loader code to a function

--- data_loader.py
+++ data_loader.py
@@ -4,19 +4,21 @@ import torchvision.transforms as transforms
 import os
 
 device = torch.device('cpu')
+def load_data():
+    transform = transforms.Compose([
+        transforms.ToTensor(),  # Transform to tensor
+        transforms.Normalize((0.5,), (0.5,))  # Min-max scaling to [-1, 1]
+    ])
 
-transform = transforms.Compose([
-    transforms.ToTensor(),  # Transform to tensor
-    transforms.Normalize((0.5,), (0.5,))  # Min-max scaling to [-1, 1]
-])
+    data_dir = os.path.join("./", 'fashion_mnist')
+    print('Data stored in %s' % data_dir)
+    trainset = torchvision.datasets.FashionMNIST(root=data_dir, train=True, download=True, transform=transform)
+    testset = torchvision.datasets.FashionMNIST(root=data_dir, train=False, download=True, transform=transform)
 
-data_dir = os.path.join("./", 'fashion_mnist')
-print('Data stored in %s' % data_dir)
-trainset = torchvision.datasets.FashionMNIST(root=data_dir, train=True, download=True, transform=transform)
-testset = torchvision.datasets.FashionMNIST(root=data_dir, train=False, download=True, transform=transform)
+    classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal',
+               'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal',
-           'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=5, shuffle=False)
+    return trainloader,testloader
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
-testloader = torch.utils.data.DataLoader(testset, batch_size=5, shuffle=False)
